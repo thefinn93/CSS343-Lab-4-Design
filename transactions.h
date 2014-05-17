5a5ad9e4cac0ee5d87c880777d0b5c3f55a02d92
Finn <thefinn93@thefinn93.com>
2014-05-17 15:51:55 -0700

    Transactions are now done

--- transactions.h
+++ transactions.h
@@ -2,7 +2,9 @@
 #define TRANSACTIONS_H
 class Transactions {
 public:
-  // Check and item out to the customer
-  checkOut(Product)
+  // Checks out product to customer
+  checkOut(Product product, Customer customer);
+  // Registers that product has been returned by customer
+  checkIn(Prodct product, Customer customer);
 }
 #endif
