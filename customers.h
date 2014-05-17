376e22d77cb519ac7555f8e4950c7222333074ad
Finn <thefinn93@thefinn93.com>
2014-05-17 15:54:18 -0700

    Added some spaces

--- customers.h
+++ customers.h
@@ -5,11 +5,14 @@ class Customers {
 public:
   // Returns the new customer's ID number on success, -1 on failure
   int add(string name);
+
   // Returns the customer with the specified ID
   Customer find(int id);
+
   // Removes a customer from the database, returns true on success
   bool remove(int id);
+
 private:
-  CustomerHashTable customerlist*;
+  CustomerHashTable customerlist;
 }
 #endif
