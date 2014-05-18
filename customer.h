8e88c1a845e22db1c55799ae399b70a03a37a692
Finn <thefinn93@thefinn93.com>
2014-05-17 18:00:41 -0700

    Moved the customerhistory functions to the customer class

--- customer.h
+++ customer.h
@@ -20,10 +20,14 @@ public:
   // Displays the specified number of items in history, or all if items is not
   // specified or set to 0
   void displayHistory(int items);
+
 private:
   string name;      // The customer's name
   int pin;          // The customer's PIN
-  CustomerHistory history;  // The customer's rental history
+  Transaction* oldestTransaction;
+  Transaction* newestTransaction;
+
+  void addTransaction(Transaction* newTransaction);
 }
 
 #endif
--- transaction.h
+++ transaction.h
@@ -4,12 +4,16 @@ class Transaction {
 public:
   // An item in the transaction ledger. product was action'd by customer.
   // Action can be either 0 (check out) or 1 (check in).
-  Transaction(Product* product, Customer* customer, int action);
+  Transaction(Product* product, Customer* customer, int type);
 
   // The product that the transction is performed upon
   Product* product;
+
   // The customer that performed the transaction
   Customer* customer;
+
+  // What type of transaction this is
+  int type;
 
   // These are going to be part of a linked list, so we need a next and previous
   Transaction* older;
